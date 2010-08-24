b1f43c6fd46c9f5b5e9dc5c07ae3dcbdb93cb8a3
metaperl <metaperl@andLinux.(none)>
2010-08-24 13:06:26 -0400

    $VERSION = '7.5';

--- lib/Parse/RecDescent/FAQ.pm
+++ lib/Parse/RecDescent/FAQ.pm
@@ -2,7 +2,7 @@ package Parse::RecDescent::FAQ;
 
 use vars qw($VERSION);
 
-$VERSION = '7.4';
+$VERSION = '7.5';
 
 1;
 
@@ -17,16 +17,13 @@ Parse::RecDescent::FAQ - the official, authorized FAQ for Parse::RecDescent.
 
 =head2 Original FAQ
 
-You can see that original document (still useful) at L<Parse::RecDescent::FAQ::Original>.
-
+You can see the original FAQ (still useful) at L<Parse::RecDescent::FAQ::Original>.
 It is a document that I grew over about a decade, but I no longer have time for editing and categorizing
 other people's posts to fit them into POD format and to collect answers.
 
 =head2 Delicious Bookmarks
 
-But I still scan Google alerts for new recdescent posts daily.
-
-You may read what I have found to be useful at my
+But I still scan Google alerts for new recdescent posts daily. You may read what I have found to be useful at my
 my parse-recdescent tagged L<delicious bookmarks|http://delicious.com/metaperl/parse-recdescent>.
 
 =head2 Mailing list
@@ -35,11 +32,11 @@ You will also find the L<mailing list archives|http://lists.perl.org/list/recdes
 to be of some help or you could subscribe to
 L<the mailing list|http://lists.perl.org/list/recdescent.html> itself.
 
-=head2 Recent hot questions
+=head1 Recent hot questions
 
 I occasionally encounter a recent hot question and will post it right here
 
-=head3 Getting the return value of the top-level rule
+=head2 Getting the return value of the top-level rule
 
 Even if you read the L<Parse::RecDescent> very closely, you will not be sure of how to get the return
 data back from a top-level rule. All the examples are C<< $parser->startrule($text) or die >> without any
